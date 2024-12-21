af26cbe9bc933f0f681910cd2c3ebf0ab45129af
Prathmesh Godse <prathmesh.g27@gmail.com>
2024-12-21 15:45:39 +0530

    Add project files.

---
+++ ISL_SocietyManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ISL_SocietyManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
